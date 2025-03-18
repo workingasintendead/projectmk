5fc4ee2dc81f51d4081d98032119cf7e075a92a1
Jackson Ruckar <workingasintendead@gmail.com>
2025-03-18 09:26:47 -0700

    got prettier working

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,22 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
 import 'bootstrap/dist/css/bootstrap.css';
-import "./globals.css";
+import './globals.css';
 import BootstrapClient from '@/components/BootstrapClient.js';
 
-
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Edgewater Landscaping",
-  description: "Take your landscaping to the edge with Edgewater Landscaping.",
+  title: 'Edgewater Landscaping',
+  description: 'Take your landscaping to the edge with Edgewater Landscaping.',
 };
 
 export default function RootLayout({
